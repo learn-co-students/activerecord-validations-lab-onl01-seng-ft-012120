8314eeb8e96c0b0366496d8145ac1f57b9ed4a06
github username <github email address>
2020-03-24 00:24:21 -0400

    Done.

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,10 @@
+class TitleValidator < ActiveModel::Validator
+  def validate(record)
+    clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+    if record.title
+      unless clickbait.any?{ |w| record.title.include?(w)}
+        record.errors[:title] << "Nobody wants to click that!"
+      end
+    end
+  end
+end
